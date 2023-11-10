125807bf80845b0d31629dd6e05ba12f6e260d6e
Alexis MOUVILLE <alexismouville@Air-de-Alexis.lan>
2023-11-10 17:00:15 +0100

    change

--- DessinOlympique/finjeu2.js
+++ DessinOlympique/finjeu2.js
@@ -1,8 +1,8 @@
+function returnToGameChoice() {
+    window.location.href = '../choixDuJeu.html';
+} 
+
 function restartGame() {
     // Assurez-vous d'ajuster l'URL pour correspondre à l'emplacement de votre fichier jeu2.html
     window.location.href = 'jeu2.html';
-}
-
-function returnToGameChoice() {
-    window.location.href = '../choixDuJeu.html';
 }
